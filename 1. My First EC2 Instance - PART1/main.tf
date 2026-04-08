1534f0fb1f6659917d7e72229274f357cfc6334e
Bzahirpour <Bzahirpour@users.noreply.github.com>
2026-04-08 16:59:50 -0400

    Update

--- 1. My First EC2 Instance - PART1/main.tf
+++ 1. My First EC2 Instance - PART1/main.tf
@@ -6,13 +6,24 @@ terraform {
     }
   }
 }
+# This grabs the latest ami to be referenced in the resource block. This is good for test/dev but not recommended for prod. in prod we can reference an ami in variables.tf file and hardcode the ami id there. 
+# This way we have more control over the ami we are using and we can test the new ami before updating the reference in variables.tf file.
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "MyFirstEC2Instance" {
-  ami           = "ami-0ea87431b78a82070"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   key_name      = "A4l-Key"
   vpc_security_group_ids = ["sg-058214e963280c39d"]
